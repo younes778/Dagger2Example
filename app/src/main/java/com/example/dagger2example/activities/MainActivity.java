c8c9f4df3a00f07677f738c9fdf94c0dd0cd4473
Younes <by_sennadj@esi.dz>
2019-03-29 00:08:12 -0500

    Last Commit

--- app/src/main/java/com/example/dagger2example/activities/MainActivity.java
+++ app/src/main/java/com/example/dagger2example/activities/MainActivity.java
@@ -1,19 +1,19 @@
 package com.example.dagger2example.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.dagger2example.R;
 import com.example.dagger2example.dagger.CarComponent;
 import com.example.dagger2example.dagger.DaggerCarComponent;
-import com.example.dagger2example.dagger.DieselEngineModule;
 import com.example.dagger2example.model.Car;
 
 import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Inject Car car;
+    @Inject
+    Car car;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
